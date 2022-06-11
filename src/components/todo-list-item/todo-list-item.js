aa8e2a1634340bff3924693ba0f8b6a7207e10ca
Rovshan Azimov <azi.rovshan>
2022-06-11 09:10:04 +0300

    git commit

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,38 +1,11 @@
 /* eslint-disable react/no-access-state-in-setstate */
 /* eslint-disable react/destructuring-assignment */
 import React from 'react';
-import formatDistanceToNow from 'date-fns/formatDistanceToNow';
 import './todo-list-item.css';
 import PropTypes from 'prop-types';
-import secondsToTime from '../secondsToTime/secondsToTime';
 
+// eslint-disable-next-line react/prefer-stateless-function
 class TodoListItem extends React.Component {
-  state = {
-    // eslint-disable-next-line id-length
-    time: { m: 0, s: 0 },
-    seconds: 0,
-  };
-
-  componentDidMount() {
-    this.timer = 0;
-  }
-
-  startTimer = () => {
-    this.timer = setInterval(this.countUp, 1000);
-  };
-
-  pauseTimer = () => {
-    clearInterval(this.timer);
-  };
-
-  countUp = () => {
-    const seconds = this.state.seconds + 1;
-    this.setState({
-      time: secondsToTime(seconds),
-      seconds,
-    });
-  };
-
   render() {
     const { label, onDeleted, onToggleImportant, onToggleDone, done, important } = this.props;
 
@@ -43,7 +16,6 @@ class TodoListItem extends React.Component {
     if (important) {
       classNames += ' important';
     }
-    const date = formatDistanceToNow(new Date(), { addSuffix: true });
 
     return (
       <div className={classNames}>
@@ -63,21 +35,6 @@ class TodoListItem extends React.Component {
           <button type="button" className="btn btn-outline-danger btn-sm float-right" onClick={onDeleted}>
             <i className="bi bi-trash" />
           </button>
-        </div>
-        <div className="time-container">
-          <div>
-            <button type="button" className="float-none icon icon-play" onClick={this.startTimer}>
-              {' '}
-            </button>
-
-            <button type="button" className="float-none icon icon-pause" onClick={this.pauseTimer}>
-              {' '}
-            </button>
-            <span className="timer-time">
-              m: {this.state.time.m} s: {this.state.time.s}
-            </span>
-          </div>
-          <span className="date-style">Generated {date}</span>
         </div>
       </div>
     );
